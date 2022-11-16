7246843d149617392f6bfb971e1b4a8bb4b983b0
George Moldoveanu <5842346+mol-george@users.noreply.github.com>
2022-11-16 16:40:27 +0000

    add guessed func

--- main.go
+++ main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
+
+// Function:
+// 1. generate a random number between 1 and 100
+// 2. ask the user to guess the number
+// 3. if the user guesses the number, print "You win!" and the number of guesses
+// 4. if the user guesses too high, print "Too high"
+// 5. if the user guesses too low, print "Too low"
+// 6. repeat until the user guesses the number
+
+func guessed(secretNumber, guess int) bool {
+	// use switch to check if the user guesses the number
+	switch {
+	case guess == secretNumber:
+		fmt.Println("You win!")
+		return true
+	case guess > secretNumber:
+		fmt.Println("Too high")
+		return false
+	case guess < secretNumber:
+		fmt.Println("Too low")
+		return false
+	}
+	return false
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	// generate a random number between 1 and 100
+	secretNumber := rand.Intn(100) + 1
+
+	// ask the user to guess the number
+	var guess int
+	fmt.Println("Guess the number between 1 and 100: ")
+	fmt.Scan(&guess)
+
+	// attempt to guess the number until the user guesses the number
+	for !guessed(secretNumber, guess) {
+		fmt.Println("Guess again: ")
+		fmt.Scan(&guess)
+	}
 }
